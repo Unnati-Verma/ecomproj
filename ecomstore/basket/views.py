07c6e92c56aef1746381c7617c54070d1ece1b97
Unnati Verma <unnatiwork18@gmail.com>
2021-08-08 19:14:48 +0530

    commit 2

--- ecomstore/basket/views.py
+++ ecomstore/basket/views.py
@@ -1,28 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 
 from store.models import Product
+
 from .basket import Basket
 
 
-
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
 
 def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product = product)
-        response = JsonResponse({'test' : 'data'})
+        basket.add(product=product, qty=product_qty)
+
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
         return response
 
-
-"""    
-    try:
-       product = Product.objects,get(id= product_id)
-    except:
-       raise Http404
-    """
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
